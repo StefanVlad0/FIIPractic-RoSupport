9f82cf0a52ed12f8ca78cf436711e731a61c47ef
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2024-04-09 17:32:05 +0300

    style messages

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -5,15 +5,21 @@
 @endsection
 
 @section('content')
-<form method="POST" action="/message/{{ $receiver->name }}">
-    @csrf
-    <textarea name="content"></textarea>
-    <button type="submit">Trimite</button>
-</form>
 
-@foreach ($messages as $message)
-    <div style="text-align: {{ $message->sender_id == auth()->id() ? 'right' : 'left' }}">
-        {{ $message->content }}
-    </div>
-@endforeach
+<div class="message-container">
+    @foreach ($messages as $message)
+        <div class="{{ $message->sender_id == auth()->id() ? 'message-right' : 'message-left' }}">
+            <p>{{ $message->content }}</p>
+        </div>
+    @endforeach
+</div>
+
+<div class="message-form">
+    <form method="POST" action="/message/{{ $receiver->name }}">
+        @csrf
+        <textarea name="content" class="message-input"></textarea>
+        <button type="submit">Trimite</button>
+    </form>
+</div>
+
 @endsection
