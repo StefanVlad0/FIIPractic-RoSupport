dc61e3616e12f860f3beac76ed37fd371681b92b
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2024-04-11 00:20:10 +0300

    Styling create post button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,16 +10,37 @@
 
 @section('content')
 <h2>Hello, {{ \Illuminate\Support\Facades\Auth::user()->name }}!</h2>
-<a href="{{ route('posts.create') }}">Create post</a>
+
 
 <div class="posts-container">
+    <a href="{{ route('posts.create') }}" class="post">
+        @if(\Illuminate\Support\Facades\Auth::user()->profile_image)
+            <div class="create-post-container">
+                <img src="{{ asset('images/' . \Illuminate\Support\Facades\Auth::user()->profile_image) }}" alt="User profile image" style="width: 35px; height: 35px; border-radius: 50%;">
+                <div class="create-post">
+                    Create a post
+                </div>
+            </div>
+        @else
+            <div class="create-post-container">
+                <div>
+                    <i class="fas fa-user-circle user-icon" style="font-size: 30px;"></i>
+                </div>
+                <div class="create-post">
+                    Create a post
+                </div>
+            </div>
+        @endif
+    </a>
 @foreach($posts as $post)
         <div class="post">
                 <a href="{{ route('users.show', $post->user->name) }}" class="user-info">
                 @if($post->user->profile_image)
                     <img src="{{ asset('images/' . $post->user->profile_image) }}" alt="User profile image" style="width: 35px; height: 35px; border-radius: 50%;">
                 @else
-                    <i class="fas fa-user-circle"></i>
+                    <div>
+                        <i class="fas fa-user-circle user-icon" style="font-size: 30px;"></i>
+                    </div>
                 @endif
                 <span><strong>{{ $post->user->name }}</strong></span>
                 </a>
