7927e687f219af709d67e3baa9175c65ee53f8f3
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2024-04-12 22:02:46 +0300

    Added styles to post and product create pages

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,19 +4,26 @@
     Create Post
 @endsection
 
-@section('content')
-    <h2>Create a new post</h2>
+@section('head')
+    <link rel="stylesheet" href="{{ asset('css/createPostStyles.css') }}">
+@endsection
 
-    <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
-        @csrf
-        <div class="form-group">
-            <label for="description">Description</label>
-            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="image">Upload Image</label>
-            <input type="file" class="form-control-file" id="image" name="image">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+@section('content')
+    <div class="post-form-container">
+        <h2>Create a new post</h2>
+
+        <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="image">Upload Image</label>
+                <input type="file" class="form-control-file" id="image" name="image">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
 @endsection
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -4,46 +4,52 @@
     Create Product
 @endsection
 
-@section('content')
-    <h2>Create a new product</h2>
+@section('head')
+    <link rel="stylesheet" href="{{ asset('css/createProductStyles.css') }}">
+@endsection
 
-    <form method="POST" action="{{ route('products.store') }}" enctype="multipart/form-data">
-        @csrf
-        <div class="form-group">
-            <label for="description">Description</label>
-            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="quantity">Quantity</label>
-            <input type="number" class="form-control" id="quantity" name="quantity">
-        </div>
-        <div class="form-group">
-            <label for="is_promoted">Is Promoted</label>
-            <select class="form-control" id="is_promoted" name="is_promoted">
-                <option value="1">Yes</option>
-                <option value="0">No</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="tags">Tags</label>
-            <select multiple class="form-control" id="tags" name="tags[]">
-                @foreach($tags as $tag)
-                    <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="image1">Upload Image 1</label>
-            <input type="file" class="form-control-file" id="image1" name="image1">
-        </div>
-        <div class="form-group">
-            <label for="image2">Upload Image 2</label>
-            <input type="file" class="form-control-file" id="image2" name="image2">
-        </div>
-        <div class="form-group">
-            <label for="image3">Upload Image 3</label>
-            <input type="file" class="form-control-file" id="image3" name="image3">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+@section('content')
+    <div class="product-form-container">
+        <h2>Create a new product</h2>
+
+        <form method="POST" action="{{ route('products.store') }}" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="quantity">Quantity</label>
+                <input type="number" class="form-control" id="quantity" name="quantity">
+            </div>
+            <div class="form-group">
+                <label for="is_promoted">Is Promoted</label>
+                <select class="form-control" id="is_promoted" name="is_promoted">
+                    <option value="1">Yes</option>
+                    <option value="0">No</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="tags">Tags</label>
+                <select multiple class="form-control" id="tags" name="tags[]">
+                    @foreach($tags as $tag)
+                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="image1">Upload Image 1</label>
+                <input type="file" class="form-control-file" id="image1" name="image1">
+            </div>
+            <div class="form-group">
+                <label for="image2">Upload Image 2</label>
+                <input type="file" class="form-control-file" id="image2" name="image2">
+            </div>
+            <div class="form-group">
+                <label for="image3">Upload Image 3</label>
+                <input type="file" class="form-control-file" id="image3" name="image3">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
